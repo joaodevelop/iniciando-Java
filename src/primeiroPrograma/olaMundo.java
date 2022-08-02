8f1c1fea432a291252e70d053b3ead81e8a21214
João Pedro Ferreira Paulino Rosa <jpfprosa@gmail.com>
2022-08-02 11:46:18 -0300

    Primeiro commit

---
+++ src/primeiroPrograma/olaMundo.java
@@ -0,0 +1,16 @@
+package primeiroPrograma;
+
+import primeiroPrograma.um.testeum;
+
+public class olaMundo {
+
+	public static void main(String[] args) {
+		
+		testeum gato = new testeum();
+		System.out.println(gato);
+
+	}
+	
+
+
+}
---
+++ src/primeiroPrograma/um/testeum.java
@@ -0,0 +1,5 @@
+package primeiroPrograma.um;
+
+public class testeum {
+
+}
